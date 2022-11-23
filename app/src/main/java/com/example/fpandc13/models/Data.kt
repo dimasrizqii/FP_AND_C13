ffcafc32f8f1c81d7d5c91bba3f98dba170bb35e
dimasrizqii <akbarabi72@gmail.com>
2022-11-23 20:02:58 +0700

    Setup Project

---
+++ app/src/main/java/com/example/fpandc13/models/Data.kt
@@ -0,0 +1,2 @@
+package com.example.fpandc13.models
+
--- app/src/main/java/com/example/fpandc13/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/fpandc13/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fpandc13
+package com.example.fpandc13.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fpandc13.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
