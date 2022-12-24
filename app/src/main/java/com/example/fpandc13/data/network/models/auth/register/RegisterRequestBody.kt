5c8098f11c3a5cfea2ec4ad91d2bd2c4d74203ed
dimasrizqii <akbarabi72@gmail.com>
2022-12-24 23:19:42 +0700

    Implementing Authentication API

--- app/src/main/java/com/example/fpandc13/data/network/models/auth/register/RegisterRequestBody.kt
+++ app/src/main/java/com/example/fpandc13/data/network/models/auth/register/RegisterRequestBody.kt
@@ -4,8 +4,24 @@ package com.example.fpandc13.data.network.models.auth.register
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRequestBody(
+    @SerializedName("address")
+    val address: String? = null,
+    @SerializedName("country_code")
+    val countryCode: String? = null,
     @SerializedName("email")
-    val email: String?,
+    val email: String? = null,
+    @SerializedName("firstName")
+    val firstName: String? = null,
+    @SerializedName("lastName")
+    val lastName: String? = null,
     @SerializedName("password")
-    val password: String?
+    val password: String? = null,
+    @SerializedName("phone_number")
+    val phoneNumber: String? = null,
+    @SerializedName("photo")
+    val photo: String? = null,
+    @SerializedName("role")
+    val role: Int? = null,
+    @SerializedName("username")
+    val username: String? = null
 )
