45dde1ef2fcf56b06985db49ba0282e598f7132a
widi <widiyulianti2002@gmail.com>
2022-12-31 11:14:05 +0700

    detail tiket xml

--- app/src/main/java/com/example/fpandc13/ui/home/ticketdetail/TicketDetailFragment.kt
+++ app/src/main/java/com/example/fpandc13/ui/home/ticketdetail/TicketDetailFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fpandc13.ui.home.ticketList
+package com.example.fpandc13.ui.home.ticketdetail
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,14 +9,14 @@ import com.example.fpandc13.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TicketListFragment : Fragment() {
+class TicketDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket_list, container, false)
+        return inflater.inflate(R.layout.fragment_ticket_detail, container, false)
     }
 
 }
